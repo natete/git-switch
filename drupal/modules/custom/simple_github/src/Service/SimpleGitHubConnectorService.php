32fa05a03dc5c94959e596759da346862c1831ec
Carlos Raigada <craigada@emergya.com>
2017-03-09 14:53:52 +0100

    Refactor curl request

--- drupal/modules/custom/simple_github/src/Service/SimpleGitHubConnectorService.php
+++ drupal/modules/custom/simple_github/src/Service/SimpleGitHubConnectorService.php
@@ -21,7 +21,7 @@ class SimpleGitHubConnectorService {
 
     const CLIENT_ID ="cf0f72380b77a0ae16e9";
     const CLIENT_SECRET = "c6962314dc7945e8f2f09888d6ee61c352e867c8";
-
+    const BASE_URL = "https://api.github.com";
     protected $response;
     protected $response_status;
     protected $access_token;
@@ -42,27 +42,8 @@ class SimpleGitHubConnectorService {
             "state" => $state
         );
 
-//        $this->response = $this->request('POST', $url, $parameters);
-//        ?access_token=e72e16c7e42f292c6912e7710c838347ae178b4a&scope=user%2Cgist&token_type=bearer
-        $fields_string = "";
-        //Parsing for curl syntax
-        foreach($parameters as $key=>$value) {
-            $fields_string .= $key.'='.$value.'&';
-        }
-        rtrim($fields_string,'& ');
-
-        //Open curl stream
-        $ch = curl_init();
-
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_POST, count($parameters));
-        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-        $response = curl_exec($ch);
-
-        //Close curl stream
-        curl_close($ch);
+        //Call curl method saving the response
+        $response = $this->sendCurlRequest(POST, $url, $parameters);
 
         //Parse get-type response
         parse_str($response['query'], $response_params);
@@ -72,5 +53,65 @@ class SimpleGitHubConnectorService {
 
         //Return the obtained token
         return $this->access_token;
+    }
+
+    public function getRepositoriesByUser($user){
+
+        $url = BASE_URL."/user/repos";
+        $parameters = array(
+            "visibility" => "all",
+            "affiliation" => "owner,collaborator,organization_member",
+            "type" => "",
+            "sort" => "updated",
+            "direction" => "desc"
+        );
+
+        $response = $this->sendCurlRequest(GET, $url, $parameters,$user);
+
+        $repositories_list = "";
+
+        return $repositories_list;
+    }
+
+    private function sendCurlRequest($rest_method,$url,$parameters,$user){
+
+        //Parsing for curl syntax
+        $fields_string = "";
+        foreach($parameters as $key=>$value) {
+            $fields_string .= $key.'='.$value.'&';
+        }
+        //Delete last ampersand
+        rtrim($fields_string,'&');
+
+        //Open curl stream
+        $ch = curl_init();
+
+        if($user){
+            curl_setopt($ch, CURLOPT_USERPWD, "$user->username:$user->token");
+        }
+
+        //Set Url
+        curl_setopt($ch,CURLOPT_URL, $url);
+
+        //Uppercasing the method name to compare and evaluate
+        $method = strtoupper($rest_method);
+
+        if($method === 'POST'){ //For post requests
+            curl_setopt($ch,CURLOPT_POST, count($parameters));
+        }
+
+        if($method === 'GET'){ //For get requests
+            curl_setopt($ch,CURLOPT_HTTPGET, true);
+        }
+        //Set parameters
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+        //Execute request and save value
+        $response = curl_exec($ch);
+
+        //Close curl stream
+        curl_close($ch);
+
+        return $response;
     }
 }
