dda814be9b1af257331f20ef585f9ffeb7d528e8
Alejandro Gómez Morón <amoron@emergya.com>
2017-03-10 11:32:02 +0100

    Appliying drupal format

--- drupal/modules/custom/simple_github/src/Controller/SimpleGitHubController.php
+++ drupal/modules/custom/simple_github/src/Controller/SimpleGitHubController.php
@@ -62,77 +62,78 @@ class SimpleGitHubController extends ControllerBase implements ContainerInjectio
     return new static($user_data, $link_generator);
   }
 
-    /**
-     * Returns the list of repositories for a user.
-     *
-     * @param \Drupal\user\UserInterface $user
-     *   A user account object.
-     *
-     * @return string
-     *   A HTML-formatted string with the list of repositories.
-     */
-    public function repositories(UserInterface $user) {
-        $list = array();
-        $connector = new SimpleGitHubConnectorService;
-        $code = "89f0cdc0a73061472248";
-        $state = "DlFPJIsvf7FOmBP6R8PPukX0igPgKoymBbRcFt5G";
+  /**
+   * Returns the list of repositories for a user.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   A user account object.
+   *
+   * @return string
+   *   A HTML-formatted string with the list of repositories.
+   */
+  public function repositories(UserInterface $user) {
+    $list = array();
+    $connector = new SimpleGitHubConnectorService;
+    $code = "89f0cdc0a73061472248";
+    $state = "DlFPJIsvf7FOmBP6R8PPukX0igPgKoymBbRcFt5G";
 
-/*
-        $list['#cache']['tags'] = array(
-            'simple_github:' => $user->id(),
-        );
-*/
-        //$list['heading']['#markup'] = $this->linkGenerator->generate($this->t('Add consumer'), Url::fromRoute('oauth.user_consumer_add', array('user' => $user->id())));
-
-        // Get the list of repositories.
-        $result = $this->user_data->get('simple_github', $user->id(), 'repositories');
-
-        // Define table headers.
-        $list['table'] = array(
-            '#theme' => 'table',
-            '#header' => array(
-                'repo_name' => array(
-                    'data' => $this->t('Repository name'),
-                ),
-                'repo_location' => array(
-                    'data' => $this->t('Repository location'),
-                ),
-                'operations' => array(
-                    'data' => $this->t('Operations'),
-                ),
-                'client' => array(
-                    'data' => $this->t('Client'),
-                ),
-            ),
-            '#rows' => array(),
-        );
-
-        // Add existing repositories to the table.
-        //foreach ($result as $repository) {
-            $list['table']['#rows'][] = array(
-                'data' => array(
-                    'repo_name' => 'Prueba',//$repository['repo_name'],
-                    'repo_location' => 'Aqui',//$repository['repo_location'],
-                    'operations' => array(
-                        'data' => array(
-                            '#type' => 'operations',
-                            '#links' => array(
-                                'delete' => array(
-                                    'title' => $this->t('Delete'),
-                                    'url' => '#'//Url::fromRoute('simple_github.user_consumer_delete', array('user' => $user->id(), 'key' => $key)),
-                                ),
-                            ),
-                        ),
-                    ),
-                    'client' => $connector->getAccessToken($code,$state),
-                ),
+    /*
+            $list['#cache']['tags'] = array(
+                'simple_github:' => $user->id(),
             );
-       // }
+    */
+    //$list['heading']['#markup'] = $this->linkGenerator->generate($this->t('Add consumer'), Url::fromRoute('oauth.user_consumer_add', array('user' => $user->id())));
+
+    // Get the list of repositories.
+    $result = $this->user_data->get('simple_github', $user->id(), 'repositories');
+
+    // Define table headers.
+    $list['table'] = array(
+      '#theme' => 'table',
+      '#header' => array(
+        'repo_name' => array(
+          'data' => $this->t('Repository name'),
+        ),
+        'repo_location' => array(
+          'data' => $this->t('Repository location'),
+        ),
+        'operations' => array(
+          'data' => $this->t('Operations'),
+        ),
+        'client' => array(
+          'data' => $this->t('Client'),
+        ),
+      ),
+      '#rows' => array(),
+    );
+
+    // Add existing repositories to the table.
+    //foreach ($result as $repository) {
+    $list['table']['#rows'][] = array(
+      'data' => array(
+        'repo_name' => 'Prueba',//$repository['repo_name'],
+        'repo_location' => 'Aqui',//$repository['repo_location'],
+        'operations' => array(
+          'data' => array(
+            '#type' => 'operations',
+            '#links' => array(
+              'delete' => array(
+                'title' => $this->t('Delete'),
+                'url' => '#'
+                //Url::fromRoute('simple_github.user_consumer_delete', array('user' => $user->id(), 'key' => $key)),
+              ),
+            ),
+          ),
+        ),
+        'client' => $connector->getAccessToken($code, $state),
+      ),
+    );
+    // }
 
 
-        $list['table']['#empty'] = $this->t('There are no repositories.');
+    $list['table']['#empty'] = $this->t('There are no repositories.');
 
-        return $list;
-    }
+    return $list;
+  }
 
 }
