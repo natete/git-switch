a9a054b207cc591b3dc867a732c53e95dbba2b52
Carlos Raigada <craigada@emergya.com>
2017-03-13 10:01:12 +0100

    Format drupal way

--- drupal/modules/custom/simple_github/src/Service/SimpleGitHubConnectorService.php
+++ drupal/modules/custom/simple_github/src/Service/SimpleGitHubConnectorService.php
@@ -19,124 +19,124 @@ use Symfony\Component\HttpFoundation\Request;
 
 class SimpleGitHubConnectorService {
 
-const CLIENT_ID = "cf0f72380b77a0ae16e9";
-const CLIENT_SECRET = "c6962314dc7945e8f2f09888d6ee61c352e867c8";
+  const CLIENT_ID = "cf0f72380b77a0ae16e9";
+  const CLIENT_SECRET = "c6962314dc7945e8f2f09888d6ee61c352e867c8";
 
-protected $response;
-protected $response_status;
-protected $access_token;
+  protected $response;
+  protected $response_status;
+  protected $access_token;
 
-public function getAccessToken($code, $state) {
+  public function getAccessToken($code, $state) {
 // AVANGELIO: https://gist.github.com/aaronpk/3612742
-$config = \Drupal::config('simple_github.settings');
+    $config = \Drupal::config('simple_github.settings');
 
 //Url to attack
-$url = "https://github.com/login/oauth/access_token";
+    $url = "https://github.com/login/oauth/access_token";
 
 //Set parameters
-$parameters = array(
-"client_id" => 'cf0f72380b77a0ae16e9',
-"client_secret" => 'c6962314dc7945e8f2f09888d6ee61c352e867c8',
-"code" => $code,
-"redirect_uri" => "",
-"state" => $state
-);
+    $parameters = array(
+      "client_id" => 'cf0f72380b77a0ae16e9',
+      "client_secret" => 'c6962314dc7945e8f2f09888d6ee61c352e867c8',
+      "code" => $code,
+      "redirect_uri" => "",
+      "state" => $state
+    );
 
 //        $this->response = $this->request('POST', $url, $parameters);
 //        ?access_token=e72e16c7e42f292c6912e7710c838347ae178b4a&scope=user%2Cgist&token_type=bearer
 
 //Open curl stream
-$ch = $this->getConfiguredCURL($url);
+    $ch = $this->getConfiguredCURL($url);
 //set the url, number of POST vars, POST data
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, count($parameters));
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, count($parameters));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
 
 
-$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
-$response = curl_exec($ch);
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
+    $response = curl_exec($ch);
 
 //Close curl stream
-curl_close($ch);
+    curl_close($ch);
 
 //Parse get-type response
 
 
-error_log('>>>' . print_r(json_decode($response), TRUE));
+    error_log('>>>' . print_r(json_decode($response), TRUE));
 //Exposing the access token if it's necessary
-$this->access_token = $response['access_token'];
-$this->token_type = $response['token_type'];
+    $this->access_token = $response['access_token'];
+    $this->token_type = $response['token_type'];
 //    error_log('>>>'.print_r(json_decode($this->access_token), true));
 //Return the obtained token3
-return $this->access_token;
-}
+    return $this->access_token;
+  }
 
-public function getRepositoriesByUser($user) {
+  public function getRepositoriesByUser($user) {
 
-$url = "https://api.github.com/user/repos";
-$ch = $this->getConfiguredCURL($url, $user);
-$response = curl_exec($ch);
+    $url = "https://api.github.com/user/repos";
+    $ch = $this->getConfiguredCURL($url, $user);
+    $response = curl_exec($ch);
 
-return $response;
-}
+    return $response;
+  }
 
-public function getRepositoryByName($user, $name){
-  $url = "https://api.github.com/".$user->username."/".$name;
-  $ch = $this->getConfiguredCURL($url, $user);
-  $response = curl_exec($ch);
+  public function getRepositoryByName($user, $name) {
+    $url = "https://api.github.com/" . $user->username . "/" . $name;
+    $ch = $this->getConfiguredCURL($url, $user);
+    $response = curl_exec($ch);
 
-  return $response;
-}
+    return $response;
+  }
 
-public function getPullRequestList($user,$repo){
-$url = "https://api.github.com/repos/".$user->username."/".$repo."/pulls";
-$ch = $this->getConfiguredCURL($url, $user);
-$response = curl_exec($ch);
+  public function getPullRequestList($user, $repo) {
+    $url = "https://api.github.com/repos/" . $user->username . "/" . $repo . "/pulls";
+    $ch = $this->getConfiguredCURL($url, $user);
+    $response = curl_exec($ch);
 
-return $response;
-}
+    return $response;
+  }
 
-public function getPullRequestById($user,$repo,$id){
-$url = "https://api.github.com/repos/".$user->username."/".$repo."/pulls/".$id;
-$ch = $this->getConfiguredCURL($url, $user);
-$response = curl_exec($ch);
+  public function getPullRequestById($user, $repo, $id) {
+    $url = "https://api.github.com/repos/" . $user->username . "/" . $repo . "/pulls/" . $id;
+    $ch = $this->getConfiguredCURL($url, $user);
+    $response = curl_exec($ch);
 
-return $response;
-}
+    return $response;
+  }
 
-public function getUserDetail($user) {
-$url = "https://api.github.com/users/" . $user->username;
-$ch = $this->getConfiguredCURL($url, $user);
-$response = curl_exec($ch);
+  public function getUserDetail($user) {
+    $url = "https://api.github.com/users/" . $user->username;
+    $ch = $this->getConfiguredCURL($url, $user);
+    $response = curl_exec($ch);
 
-return $response;
-}
+    return $response;
+  }
 
-protected function getHeaders() {
-$headers[] = 'Accept: application/json';
+  protected function getHeaders() {
+    $headers[] = 'Accept: application/json';
 // if we have the security token
-if (!empty($this->token_type)) {
-$headers[] = 'Bearer ' . $this->access_token;
-}
-return $headers;
-}
+    if (!empty($this->token_type)) {
+      $headers[] = 'Bearer ' . $this->access_token;
+    }
+    return $headers;
+  }
 
-protected function getConfiguredCURL($url, $user = NULL) {
-$ch = curl_init($url);
+  protected function getConfiguredCURL($url, $user = NULL) {
+    $ch = curl_init($url);
 //set the url, number of POST vars, POST data
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-$headers = $this->getHeaders();
+    $headers = $this->getHeaders();
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-if ($user != NULL) {
-curl_setopt($ch, CURLOPT_USERPWD, "$user->username:$user->token");
-}
+    if ($user != NULL) {
+      curl_setopt($ch, CURLOPT_USERPWD, "$user->username:$user->token");
+    }
 
-return $ch;
-}
+    return $ch;
+  }
 }
