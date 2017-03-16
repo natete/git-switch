094d4e7a93ab80ec5663a7f366f3f89d2cf35e19
Estefania Barrera <ebarrera@emergya.com>
2017-03-16 11:59:43 +0100

    changed businessLogic

--- drupal/modules/custom/simple_git/src/BusinessLogic/SimpleGitDataBaseBusinnesLogic.php
+++ drupal/modules/custom/simple_git/src/BusinessLogic/SimpleGitDataBaseBusinnesLogic.php
@@ -1,15 +1,25 @@
 <?php
+
 /**
  * @file
  * Contains \Drupal\simple_git\BusinessLogic\SimpleGitDataBaseBusinnesLogic
  */
 abstract class SimpleGitDataBaseBusinnesLogic {
 
+  /**
+   * @param $user
+   * @return mixed
+   */
   static final function getAccounts($user) {
     return Drupal::service('user.data')
       ->get(MODULE_SIMPLEGIT, $user->id(), 'accounts');
   }
 
+  /**
+   * @param $user
+   * @param $accounts
+   * @return mixed
+   */
   static final function setAccounts($user, $accounts) {
     return Drupal::service('user.data')
       ->set(MODULE_SIMPLEGIT, $user->id(), 'accounts', $accounts);;
